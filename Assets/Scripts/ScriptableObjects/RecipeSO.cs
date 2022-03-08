964aa302428af6b7db8ab4207e108fbc66fd86b3
Gautier <g.debreu@rubika-edu.com>
2022-03-08 13:52:51 +0100

    Book Panel and Station, RecipeSO

---
+++ Assets/Scripts/ScriptableObjects/RecipeSO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Recipe", menuName = "ScriptableObjects/Recipe")]
+public class RecipeSO : ScriptableObject
+{
+    [Header("Global Infos")] 
+    public new string name;
+    public ItemType itemType;
+    
+    [Header("Sprites")]
+    public Sprite foodSprite;
+
+    [Header("Attributes")] 
+    public List<RecipeElement> recipeElements;
+    public int price;
+    
+}
+
+[Serializable]
+public class RecipeElement
+{
+    public ItemType itemType;
+    public int amount;
+}
