416ac797b2f591e482863ce844c89941cba30656
Gautier <g.debreu@rubika-edu.com>
2022-03-08 13:52:51 +0100

    Book Panel and Station, RecipeSO

---
+++ Assets/Scripts/Kitchen/BookPanel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookPanel : MonoBehaviour
+{
+    [SerializeField] private List<RecipeSO> recipes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
