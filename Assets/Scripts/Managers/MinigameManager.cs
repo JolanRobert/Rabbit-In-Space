f17c5eed54a5458b3573893af73d0b865c8be811
Gautier <g.debreu@rubika-edu.com>
2022-03-23 17:25:23 +0100

    Minigame Tool

--- Assets/Scripts/Managers/MinigameManager.cs
+++ Assets/Scripts/Managers/MinigameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MinigameManager : MonoBehaviour {
     
     public static MinigameManager instance;
+    public bool resultPending;
     
     void Awake() {
         instance = this;
@@ -14,9 +15,15 @@ public class MinigameManager : MonoBehaviour {
         //Hide customers
         KitchenManager.Instance.transform.position += Vector3.left*100;
         RecipeManager.instance.HideRecipeTimeline();
+        resultPending = true;
     }
     
     public void EndMinigame(bool success) {
+        if(!resultPending) return;
+        resultPending = false;
+        
+        SwitchScene.Instance.ToKitchen();
+        
         //Show customers
         KitchenManager.Instance.transform.position -= Vector3.left*100;
         
