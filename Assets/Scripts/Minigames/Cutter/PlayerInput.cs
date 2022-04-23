b9a82a65947d31b10f90d76e00bc808e1d777343
Gautier <g.debreu@rubika.com>
2022-04-23 18:28:51 +0200

    Trail in cutter

--- Assets/Scripts/Minigames/Cutter/PlayerInput.cs
+++ Assets/Scripts/Minigames/Cutter/PlayerInput.cs
@@ -5,14 +5,21 @@ namespace Cutter {
     public class PlayerInput : MonoBehaviour {
         
         private Camera mainCamera;
+        [SerializeField] private TrailRenderer trailRenderer;
 
         private void Start() {
             mainCamera = Camera.main;
         }
     
         private void Update() {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                transform.position = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,-mainCamera.transform.position.z));
+                trailRenderer.Clear();
+            }
             if (Input.GetKey(KeyCode.Mouse0)) {
                 ScreenToRay(Input.mousePosition);
+                transform.position = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,-mainCamera.transform.position.z));
             }
         }
 
