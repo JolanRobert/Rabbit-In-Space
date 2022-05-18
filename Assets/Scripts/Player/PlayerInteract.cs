5c735832137fb821c723a1e4c051fbde8476624c
Jolan <j.robert@rubika-edu.com>
2022-05-18 12:24:33 +0200

    Transitions

--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -9,6 +9,7 @@ public class PlayerInteract : MonoBehaviour {
     public bool isInteracting;
     
     public void TryInteract(IInteractable interactableGO) {
+        Debug.Log(interactableGO.name);
         if (isTryingToInteract) StopCoroutine(interactCR);
         interactCR = StartCoroutine(InteractCR(interactableGO));
     }
--- Assets/Scripts/Utils/SwitchScene.cs
+++ Assets/Scripts/Utils/SwitchScene.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour {
 
     public static SwitchScene Instance;
+
+    [SerializeField] private CanvasGroup loadingScreen;
+    [SerializeField] private float loadingAnimationTime;
 
     void Awake() {
         if (Instance != null) Destroy(gameObject);
@@ -15,14 +19,19 @@ public class SwitchScene : MonoBehaviour {
     }
     
     public void ChangeScene(string newScene) {
-        StartCoroutine(LoadAsyncScene(newScene));
+        StartCoroutine(LoadScene(newScene));
     }
 
-    private IEnumerator LoadAsyncScene(string nextScene) {
+    public IEnumerator LoadScene(string nextScene) {
+        //Play animation
+        loadingScreen.DOFade(1, loadingAnimationTime);
+        yield return new WaitForSeconds(loadingAnimationTime);
+        
+        //Load scene
         AsyncOperation operation = SceneManager.LoadSceneAsync(nextScene);
-
-        while (!operation.isDone) {
-            yield return null;
-        }
+        while (!operation.isDone) yield return null;
+        
+        //End animation
+        loadingScreen.DOFade(0, loadingAnimationTime);
     }
 }
