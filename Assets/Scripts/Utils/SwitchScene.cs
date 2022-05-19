a5aa2683db210e5486cd06c903d63049e521c639
Gautier <g.debreu@rubika.com>
2022-05-19 11:02:34 +0200

    Save player position and rotation between scenes

--- Assets/Scripts/Utils/SwitchScene.cs
+++ Assets/Scripts/Utils/SwitchScene.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 public class SwitchScene : MonoBehaviour {
 
     public static SwitchScene Instance;
+    private Vector3 playerPos;
+    private Quaternion playerRotation;
 
     void Awake() {
         if (Instance != null) Destroy(gameObject);
@@ -14,7 +16,13 @@ public class SwitchScene : MonoBehaviour {
         }
     }
     
-    public void ChangeScene(string newScene) {
+    public void ChangeScene(string newScene)
+    {
+        if (SceneManager.GetActiveScene().name == "Kitchen")
+        {
+            playerPos = PlayerManager.Instance.transform.position;
+            playerRotation = PlayerManager.Instance.transform.rotation;
+        }
         StartCoroutine(LoadAsyncScene(newScene));
     }
 
@@ -23,6 +31,12 @@ public class SwitchScene : MonoBehaviour {
 
         while (!operation.isDone) {
             yield return null;
+        }
+        
+        if (nextScene == "Kitchen" && playerPos != default)
+        {
+            PlayerManager.Instance.GetMovement().Teleport(playerPos);
+            PlayerManager.Instance.transform.rotation = playerRotation;
         }
     }
 }
